7304939cb1e07e80a778afcbdd294bc3cc2fd6c1
sjin <wlstjdeo00@gmail.com>
2023-03-16 19:42:59 +0900

    INIT

---
+++ src/main/java/org/example/util/Adder.java
@@ -0,0 +1,10 @@
+package org.example.util;
+
+import java.util.function.Function;
+
+public class Adder implements Function<Integr, Integer> {
+    @Override
+    public Integer apply(Integr integr) {
+        return null;
+    }
+}
